6d17d09439261434c116be0f8ef79ad6b83939aa
Cedric Kwadwo Ahenkorah <dev.ckahenkorah@epahubb.com>
2023-08-30 17:35:46 +0000

    timestamps added

--- models/Campaign.js
+++ models/Campaign.js
@@ -2,23 +2,26 @@ const mongoose = require("mongoose");
 
 const Schema = mongoose.Schema;
 
-const campaignSchema = new Schema({
-  title: {
-    type: String,
-    required: true,
+const campaignSchema = new Schema(
+  {
+    title: {
+      type: String,
+      required: true,
+    },
+    description: {
+      type: String,
+      required: true,
+    },
+    target: {
+      type: String,
+      required: true,
+    },
+    status: {
+      type: String,
+      default: "Active",
+    },
   },
-  description: {
-    type: String,
-    required: true,
-  },
-  target: {
-    type: String,
-    required: true,
-  },
-  status: {
-    type: String,
-    default: "Active",
-  },
-});
+  { timestamps: true }
+);
 
 module.exports = mongoose.model("Campaign", campaignSchema);
